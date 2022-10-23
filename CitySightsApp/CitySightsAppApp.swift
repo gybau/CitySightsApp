103b637eb83d635b26199237909e78ed957291fa
Michał Ganiebny <michalganiebny@rocketmail.com>
2022-10-23 22:03:57 +0200

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Michał Ganiebny on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
